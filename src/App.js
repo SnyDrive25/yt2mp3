a20a13fca4825210ec49a1beb8374ae1a35ca3c4
SnyDrive ~ Sunil <sunil.goulamhoussen@edu.devinci.fr>
2023-10-07 19:39:43 +0200

    ✨ Initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [file, setFile] = useState(null);
+
+  const convert = () => {
+    const VIDEO_URL = document.getElementById('url').value;
+    const FTYPE = 'mp3';
+    const url = `https://api.vevioz.com/apis/single/${FTYPE}?url=${VIDEO_URL}`;
+    setFile(url);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        YouTube to MP3 Converter
+      </h1>
+      <p>
+        Convert YouTube videos to MP3 files.
+      </p>
+      <input type="text" id="url" placeholder="Enter YouTube URL" />
+      <button onClick={convert}>
+        Convert
+      </button>
+      {
+        file &&
+        <iframe id="singleButtonApi" src={file} width="100%" height="100%" allowtransparency="true" title='url'></iframe>
+      }
     </div>
   );
 }
